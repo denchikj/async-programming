ce0470b877f592bdf8827511feb0143255ed8bd4
denchikj <igoshevdenis9876@gmail.com>
2023-06-25 13:42:43 +0500

    Thread

---
+++ Thread/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Threads
+{
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread thread = new(new ParameterizedThreadStart(WriteChar));
+
+            thread.Start('*');
+
+            for (int i = 0; i < 20; i++)
+            {
+                Console.WriteLine('-');
+                Thread.Sleep(50);
+            }            
+        }
+
+        private static void WriteChar(object? obj)
+        {
+            var item = (char?)obj;
+            for (int i = 0; i < 20; i++)
+            {
+                Console.WriteLine(item);
+                Thread.Sleep(50);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
