8a3cd8dca52d03ae1eaae63fc0e663107636c424
denchikj <igoshevdenis9876@gmail.com>
2023-06-26 09:30:58 +0500

    ChildTasks

---
+++ ChildTasks/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace ChildTasks
+{
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+            var parent = new Task(() =>
+            {
+                new Task(() =>
+                {
+                    Thread.Sleep(1000);
+                    Console.WriteLine("nested task 1 completed");
+                }, TaskCreationOptions.AttachedToParent).Start();
+
+                new Task(() =>
+                {
+                    Thread.Sleep(2000);
+                    Console.WriteLine("nested task 2 completed");
+                }, TaskCreationOptions.AttachedToParent).Start();
+
+                Thread.Sleep(200);
+                Console.WriteLine("parent task completed");
+            }, TaskCreationOptions.DenyChildAttach);
+
+            parent.Start();
+            parent.Wait();
+            Console.WriteLine("completed");
+            Console.ReadLine();
+        }
+    }
+}
